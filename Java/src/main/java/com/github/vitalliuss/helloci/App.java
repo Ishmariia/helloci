cf7048d8008b6b6cd4f2bcca0d807d316fc9e567
Mariia Ishchenko <avemavik@gmail.com>
2023-04-19 21:34:42 +0200

    commit first maven

--- Java/src/main/java/com/github/vitalliuss/helloci/App.java
+++ Java/src/main/java/com/github/vitalliuss/helloci/App.java
@@ -8,10 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello CI!" );
+
+        System.out.println( "Hello World!");
     }
     
     public static void doNothing() {
-    	// do nothing
+        System.out.println( "doNothing");
     }
 }
